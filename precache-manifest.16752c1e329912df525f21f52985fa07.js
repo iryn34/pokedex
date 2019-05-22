e84805c8cb6ded3cad95878459675c370b098b39
iryn34 <iryna.tsuper34@gmail.com>
2019-05-22 21:41:27 +0300

    Updates

---
+++ precache-manifest.16752c1e329912df525f21f52985fa07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af87ac703009a44941be4181c0708c20",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "bf921f2a624951b495b8",
+    "url": "/pokedex/static/css/main.4b36f1b7.chunk.css"
+  },
+  {
+    "revision": "cd0e6c449b738af514af",
+    "url": "/pokedex/static/js/2.dda93018.chunk.js"
+  },
+  {
+    "revision": "bf921f2a624951b495b8",
+    "url": "/pokedex/static/js/main.2fea803a.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
